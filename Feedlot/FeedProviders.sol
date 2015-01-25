27a243667a0d6500de0255dc4ff5477a558e140d
SilentCicero <silentcicero@outlook.com>
2015-01-25 14:22:45 -0500

    Update FeedProviders.sol

--- Feedlot/FeedProviders.sol
+++ Feedlot/FeedProviders.sol
@@ -7,7 +7,7 @@ contract FeedProviders
 		string32 description;
 	}
 	
-	mapping(string32 => address) name_to_address;
+	mapping(string32 => address) name_to_provider;
 	mapping(address => Provider) providers;
 	
 	function register(string32 name, string32 url, string32 description) 
@@ -16,38 +16,36 @@ contract FeedProviders
 		new_provider.name = name;
 		new_provider.url = url;
 		new_provider.description = description;
-		address new_name_lookup = name_to_address[name];
-		name_to_address[name] = msg.sender;
+		
+		address new_name_lookup = name_to_provider[name];
+		new_name_lookup = msg.sender;
 	}
 	
 	function unregister() 
 	{
-		Provider new_provider = providers[msg.sender];
-		address new_name_lookup = name_to_address[new_provider.name];
-		name_to_address[new_provider.name] = address(0);
-		new_provider.name = '';
-		new_provider.url = '';
-		new_provider.description = '';
+		Provider get_provider = providers[msg.sender];
+		
+		name_to_provider[get_provider.name] = address(0);
+		
+		get_provider.name = "";
+		get_provider.url = "";
+		get_provider.description = "";
 	}
 	
 	function addressOf(string32 name) constant returns (address addr)
 	{
-		address new_name_lookup = name_to_address[name];
-		addr = new_name_lookup;
-	}
-	
-	function providerOf(address addr) constant returns (Provider provider) 
-	{
-	    provider = providers[addr];
+	    address get_address = name_to_provider[name];
+	    return get_address;
 	}
 	
 	function nameOf(address addr) constant returns (string32 name) 
 	{
-		name = providers[addr].name;
+		Provider get_provider = providers[addr];
+		return get_provider.name;
 	}
 	
-	// Todo
 	function kill() 
 	{
+	   // TODO
 	}
 }
