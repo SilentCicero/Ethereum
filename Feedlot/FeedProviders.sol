77fe0516095c1c222e7e456494b5155d2dc42a37
SilentCicero <silentcicero@outlook.com>
2015-01-25 00:04:10 -0500

    Update FeedProviders.sol

--- Feedlot/FeedProviders.sol
+++ Feedlot/FeedProviders.sol
@@ -1,44 +1,44 @@
 contract FeedProviders 
 {
-    struct Provider
-    {
-        string32 name;
-        string32 url;
-        string32 description;
-    }
-    
-    mapping(string32 => address) name_to_address;
-    mapping(address => Provider) providers;
-    
+	struct Provider
+	{
+		string32 name;
+		string32 url;
+		string32 description;
+	}
+	
+	mapping(string32 => address) name_to_address;
+	mapping(address => Provider) providers;
+	
 	function register(string32 name, string32 url, string32 description) 
 	{
-	    Provider new_provider = providers[msg.sender];
-	    new_provider.name = name;
-	    new_provider.url = url;
-	    new_provider.description = description;
-	    address new_name_lookup = name_to_address[name];
-	    name_to_address[name] = msg.sender;
+		Provider new_provider = providers[msg.sender];
+		new_provider.name = name;
+		new_provider.url = url;
+		new_provider.description = description;
+		address new_name_lookup = name_to_address[name];
+		name_to_address[name] = msg.sender;
 	}
 	
 	function unregister() 
 	{
-	    Provider new_provider = providers[msg.sender];
-	    address new_name_lookup = name_to_address[new_provider.name];
-	    name_to_address[new_provider.name] = address(0);
-	    new_provider.name = '';
-	    new_provider.url = '';
-	    new_provider.description = '';
+		Provider new_provider = providers[msg.sender];
+		address new_name_lookup = name_to_address[new_provider.name];
+		name_to_address[new_provider.name] = address(0);
+		new_provider.name = '';
+		new_provider.url = '';
+		new_provider.description = '';
 	}
 	
 	function addressOf(string32 name) constant returns (address addr)
 	{
-	    address new_name_lookup = name_to_address[name];
-	    addr = new_name_lookup;
+		address new_name_lookup = name_to_address[name];
+		addr = new_name_lookup;
 	}
 	
 	function nameOf(address addr) constant returns (string32 name) 
 	{
-	    name = providers[addr].name;
+		name = providers[addr].name;
 	}
 	
 	function kill() 
